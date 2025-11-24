f51aec8580017dc43fa11599d9dadb56806f20fe
Fajar Satria <fajarsatria991@gmail.com>
2025-11-25 05:49:08 +0700

    final ver

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,19 +3,23 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from mlflow.models import infer_signature
 
+# Set experiment (tidak memulai run)
 mlflow.set_experiment("Breast_Cancer_CI")
 
+# Load data
 df = pd.read_csv("breast_preprocessed_train.csv")
 X = df.drop("diagnosis", axis=1)
 y = df["diagnosis"]
 
-with mlflow.start_run(run_name="CI_AUTO_SUCCESS", nested=True):
-    model = RandomForestClassifier(n_estimators=200, random_state=42)
-    model.fit(X, y)
-    
-    # Log model + signature
-    signature = infer_signature(X, model.predict(X))
-    mlflow.sklearn.log_model(model, "model", signature=signature)
-    
-    mlflow.log_metric("status", 1.0)
-    print("CI TRAINING SELESAI – MODEL SUDAH DI-LOG!")
+# Train model
+model = RandomForestClassifier(n_estimators=200, random_state=42)
+model.fit(X, y)
+
+# Log model tanpa start_run()
+signature = infer_signature(X, model.predict(X))
+mlflow.sklearn.log_model(model, "model", signature=signature)
+
+# Log metrics
+mlflow.log_metric("training_accuracy", model.score(X, y))
+
+print("SUCCESS: CI Training Finished")
